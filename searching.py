30e189dcb5e5ff1a845e2098e6ce715d9dc8aac7
Eliska Strojilova <strojilovaeliska@seznam.cz>
2022-04-11 13:06:40 +0200

    pattern_search

--- searching.py
+++ searching.py
@@ -29,14 +29,29 @@ def linear_search(unordered_numbers, hledane_cislo):
     output = dict()
     output["positions"] = positions
     output["count"] = count
+    # output = {"positions" : positions, "count" : count}
     return output
 
+def pattern_search(dna_sequence, hledany_vzor):
+    positions = []
+    for i in range(0, len(dna_sequence) - 2):
+        dna_substring = dna_sequence[i:i + 3]
+        if dna_substring == hledany_vzor:
+            positions.append(i)
+    mnozina_pozic = set(positions)
+
+    return mnozina_pozic
 
 def main():
     unordered_numbers = read_data("sequential.json", "unordered_numbers")
     print(unordered_numbers)
     output_dict = linear_search(unordered_numbers, 5)
     print(output_dict)
+    dna_sequence = read_data("sequential.json", "dna_sequence")
+    print(dna_sequence)
+    pozice = pattern_search(dna_sequence, "ATA")
+    print(pozice)
+
 
 
 if __name__ == '__main__':
